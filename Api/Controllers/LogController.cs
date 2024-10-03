da20c08e62e13f1cd200003b11878cd1637fea5d
Felix Zamora <flxzamora91@gmail.com>
2024-10-02 21:18:14 -0600

    en el proyecto de api se definieron los endpoint para realizar operaciones de los logs

---
+++ Api/Controllers/LogController.cs
@@ -0,0 +1,59 @@
+﻿using Api.Controllers;
+using Application.UseCases.Logs.Commands.CreateLog;
+using GetLogById = Application.UseCases.Logs.Queries.GetLogById;
+using Application.UseCases.Logs.Queries.GetLogs;
+using GetLogsByType = Application.UseCases.Logs.Queries.GetLogsByType;
+using Microsoft.AspNetCore.Mvc;
+using Application.UseCases.Logs.Queries.GetLogsByType;
+
+namespace Challenge02a.Controllers
+{
+    public class LogController : BaseController
+    {
+        [HttpPost]
+        [Route("Create")]
+        [Produces(typeof(CreateLogCommandDto))]
+        [ActionName(nameof(Create))]
+        public async Task<IActionResult> Create(CreateLogCommandModel model)
+        {
+            var command = this.Mapper.Map<CreateLogCommand>(model);
+            var result = await this.Mediator.Send(command);
+            return this.FromResult(result);
+        }
+
+        [HttpGet]
+        [Route("GetAllLogs")]
+        [Produces(typeof(GetLogsQueryDto))]
+        [ActionName(nameof(GetAllLogs))]
+        public async Task<IActionResult> GetAllLogs()
+        {
+            var query = new GetLogsQuery { QueryType = GetLogsQueryType.All, Filter = string.Empty };
+            var result = await this.Mediator.Send(query);
+            return this.FromResult(result);
+        }
+
+        [HttpGet]
+        [Route("GetLogById")]
+        [Produces(typeof(GetLogById.GetLogQueryByIdDto))]
+        [ActionName(nameof(GetLogById))]
+        public async Task<IActionResult> GetLogById([FromQuery] GetLogById.GetLogsQueryFilterModel model)
+        {
+            var query = this.Mapper.Map<GetLogById.GetLogQueryById>(model);
+            var result = await this.Mediator.Send(query);
+            return this.FromResult(result);
+        }
+
+
+        [HttpGet]
+        [Route("GetLogsByType")]
+        [Produces(typeof(GetLogsByType.GetLogQueryByTypeDto))]
+        [ActionName(nameof(GetLogsByType))]
+        public async Task<IActionResult> GetLogsByType( [FromQuery] GetLogsByType.GetLogsQueryFilterModel model)
+        {
+            var query = this.Mapper.Map<GetLogQueryByType>(model);
+            var result = await this.Mediator.Send(query);
+            return this.FromResult(result);
+        }
+
+    }
+}
--- Api/Program.cs
+++ Api/Program.cs
@@ -1,10 +1,29 @@
+using Application;
+using Infrastructure;
+using Shared.Core;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+// Add services to the container.
+EngineContext.Create();
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
 var app = builder.Build();
+
+EngineContext.Current.Configure(app.Services);
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 
