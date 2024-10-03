c35c81718b357c8c4c205781e82dd168b06ee1c9
Felix Zamora <flxzamora91@gmail.com>
2024-10-02 19:47:26 -0600

    se implemento el patron singleton para permitir solo la creacion una instancia

---
+++ Shared/Core/Singleton.cs
@@ -0,0 +1,13 @@
+﻿namespace Shared.Core
+{
+    public class Singleton<T>
+    {
+        private static T instance;
+
+        public static T Instance 
+        {
+            get => instance;
+            set { instance = value; }
+        }
+    }
+}
