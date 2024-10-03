a740ae0dbcc1ada0cae3a7ee7feb52ee50709eda
Felix Zamora <flxzamora91@gmail.com>
2024-10-02 21:12:34 -0600

    se agregaron la clase repositorio donde se definen las operaciones a la base de datos

---
+++ Infrastructure/Common/Factories/IDbContextFactory.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Common.Factories
+{
+    public interface IDbContextFactory
+    {
+        DbContext Create();
+    }
+}
---
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Application.Common.Interfaces;
+
+namespace Infrastructure.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :DbContext(options) , IApplicationDbContext
+    {
+
+    }
+}
